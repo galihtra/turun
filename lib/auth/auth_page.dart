39514fbcfe7c242c89b77e34fdde21f70669c840
galihtra <galihtririskyandiko@gmail.com>
2025-08-12 13:56:21 +0700

    sign in & sign up

---
+++ lib/auth/auth_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../home/home_page.dart';
+import '../home/start_page.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  User? _user;
+  @override
+  void initState() {
+    _getAuth();
+    super.initState();
+  }
+
+  Future<void> _getAuth() async {
+    setState(() {
+      _user = supabase.auth.currentUser;
+    });
+    supabase.auth.onAuthStateChange.listen((event) {
+      setState(() {
+        _user = event.session?.user;
+      });
+    });
+  }
+
+  final SupabaseClient supabase = Supabase.instance.client;
+  @override
+  Widget build(BuildContext context) {
+    return _user == null ? const StartPage() : const HomePage();
+  }
+}
